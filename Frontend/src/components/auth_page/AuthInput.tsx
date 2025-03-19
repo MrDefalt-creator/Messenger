46f7c163a5f58c18e69705a42560691591d4346d
Ivan <defalt06@mail.ru>
2025-03-19 21:27:51 +0300

    continue working on AuthInput.tsx, not enough time, have an idea with styles

--- Frontend/src/components/auth_page/AuthInput.tsx
+++ Frontend/src/components/auth_page/AuthInput.tsx
@@ -3,14 +3,13 @@
 export default function AuthInput({children}: {children: React.ReactNode}) {
     return(
         <div className='flex relative w-full'>
-            <span className='absolute top-2 left-3.5 text-base font-sans px-1
+            <span className='absolute top-3 left-3.5 text-base font-sans px-1
             bg-white pointer-events-none'>
                 {children}
             </span>
             <input
-                className='w-full p-2 border-2 rounded-xl border-teal-500
-                font-sans z-10
-                focus:outline-none focus:border-teal-600 focus:ease-in-out'/>
+                className='auth-input'
+                />
         </div>
     )
 }
